54da56285754c1bf816955cae29c70781053ecfa
Abhinav <abhi.gupta8802@gmail.com>
2022-10-30 17:48:53 +0530

    added all the cases there can be in the menu

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,68 @@
 import java.util.Scanner;
+import java.io.*;
+
+public class accounts{
+    
+}
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //welcome message
         Scanner scn = new Scanner(System.in);
-        System.out.print("WELCOME TO IIT TIRUPATI BANKING SERVICE");
+        System.out.println("WELCOME TO IIT TIRUPATI BANKING SERVICE");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.print("\r");
-        System.out.print("We are glad to be at your service!");
+        System.out.println("We are glad to be at your service!");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.print("\r");
 
         //taking account number input
         int tries = 3;
-        int accno = 0;
-        while(accno < 9999 || accno > 99999 || tries > 0){
-            System.out.print("Please type in your 5 - digit account number: ");
-            accno = scn.nextInt();
+        int acc = 0;
+        while(acc < 9999 || acc > 99999 || tries > 0){
+            System.out.println("Please type in your 5 - digit account number: ");
+            acc = scn.nextInt();
             tries--;
-            if(accno < 9999 || accno > 99999) System.out.println("Please enter valid account number!");
+            if(acc < 9999 || acc > 99999) System.out.print("Please enter valid account number!");
             else tries = 0;
         }
 
         //taking pin input
         tries = 3;
-        int pinno = 0;
+        int pin = 0;
+        System.out.println("Please type in you pin: ");
+        pin = scn.nextInt();
 
+        //checking if account number and pin are correct
+        //to be done
+        
+        //taking user choice 
+        System.out.println("Please select the appropriate option: ");
+        System.out.println("1)Withdraw money\n2)Deposit Money\n3)View Bank Balance\n4)Change account PIN");
+        int choice = scn.nextInt();
+
+        switch(choice){
+            case 1:
+                System.out.println("Enter the amount you want to withdraw: ");
+                int amount = scn.nextInt();
+                break;
+            case 2:
+                System.out.println("Enter the amount you want to deposit: ");
+                int amount = scn.nextInt();
+                break;
+            case 3:
+                System.out.println("You bank balance is: ");
+                break;
+            case 4:
+                System.out.println("Please enter the new pin: ");
+                int newpin = scn.nextInt();
+                break;
+        }
     }
 }
